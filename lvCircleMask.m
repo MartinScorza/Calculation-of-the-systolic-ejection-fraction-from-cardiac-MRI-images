4b1f115109250fe4b1ff47fef302a2dcfa6d09ea
Fedefioriti <ffioriti10@gmail.com>
2026-01-03 01:07:33 +0100

    General commentaries of the code

--- lvCircleMask.m
+++ lvCircleMask.m
@@ -1,37 +1,53 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Function lvCircleMask(imgBin, rRange, sensitivity, figureName)
+%%% - imgBin = The binary image
+%%% - rRange = Range of the radius
+%%% - sensitivity = How many circles you want to detect
+%%% - figureName
+%%%
+%%% - maskCircle = The mask with the values of the circle
+%%% - center = The center of the circle
+%%% - radius = The radius of the circle
+%%% - metric = Confidence score of the selected circle
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 function [maskCircle, center, radius, metric] = lvCircleMask(imgBin, rRange, sensitivity, figureName)
 
-    % --- Defaults (para que NUNCA quede sin asignar) ---
+    % Defaults (To avoid empty variables)
     [rows, cols] = size(imgBin);
     maskCircle = false(rows, cols);
     center = [NaN NaN];
     radius = NaN;
     metric = NaN;
 
-    % --- Buscar círculos ---
+    % To find the circle between the rRange of the picture that is
+    % brighter than the background and using the sensitivity given.
     [centers, radii, metrics] = imfindcircles(imgBin, rRange, ...
         'ObjectPolarity','bright', 'Sensitivity', sensitivity);
-
+    
+    % In case there is no circle detected
     if isempty(centers)
-        % opcional: mensaje debug
         warning("No circle found in %s (r=[%g %g], sens=%g). Returning empty mask.", ...
             figureName, rRange(1), rRange(2), sensitivity);
-        return;
+        return; % We return the default values
     end
 
-    % Elegir el "mejor" según tu criterio (más a la derecha)
+    % We chose the circle that is farthest to the right.
     [~, idx] = max(centers(:,1));
+    % We save the metrics, radius and center of the circle
     metric = metrics(idx);
     center = centers(idx,:);
     radius = radii(idx);
 
-    % Debug plot
+    % We declare a figure and show the binary image with the circle
+    % detected in colour
     figure('Name', ['DEBUG - ' figureName], 'NumberTitle','off');
     imshow(imgBin, []); hold on;
     viscircles(center, radius, 'EdgeColor','b');
-    title(['Círculo detectado - ' figureName]);
+    title(['C?rculo detectado - ' figureName]);
     hold off;
 
-    % Construir máscara rellena
+    % Construction of the binary mask that shows the pixels of the circle
     [X, Y] = meshgrid(1:cols, 1:rows);
     maskCircle = (X - center(1)).^2 + (Y - center(2)).^2 <= radius^2;
 end
